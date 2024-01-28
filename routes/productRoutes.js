c0707909da1739bf6a3e2f4b4099fd0ad991c3ab
AmarNathPrajapati <amarnathjnp2002@gmail.com>
2024-01-28 21:57:45 +0530

    product API Completed

--- routes/productRoutes.js
+++ routes/productRoutes.js
@@ -3,12 +3,25 @@ import express from "express";
 
 import formidable from "express-formidable";
 import { isAdmin, requireSignIn } from "../middleware/authMiddleware.js";
-import { createProductController } from "../controllers/productController.js";
+import { createProductController, deleteProductController, getProductController, getSingleProductController, productPhotoController, updateProductController } from "../controllers/productController.js";
 
 const router = express.Router();
 
 //routes
-router.post("/create-product",requireSignIn,isAdmin,formidable(),createProductController
-);
+router.post("/create-product",requireSignIn,isAdmin,formidable(),createProductController);
 
+//get products
+router.get("/get-product",getProductController)
+
+//single product
+router.get("/get-product/:slug", getSingleProductController);
+
+//get photo
+router.get("/product-photo/:pid", productPhotoController);
+
+//delete product
+router.delete("/delete-product/:pid", deleteProductController);
+
+//update product 
+router.put("/update-product/:pid",requireSignIn,isAdmin,formidable(),updateProductController);
 export default router;
